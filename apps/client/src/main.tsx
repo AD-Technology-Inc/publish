c060224af9ec2bbadd5bfc61e7e9c1a4f43dc7b1
Angelo Arcillas <angeloarcillas64@gmail.com>
2026-06-29 14:46:02 +0800

    Add design system

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,14 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { initializeTheme } from './hooks/use-appearance'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./css/app.css";
+import App from "./App.tsx";
+import { initializeTheme } from "./hooks/use-appearance";
 
 // Initialize theme on load
 initializeTheme();
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <App />
+    </StrictMode>,
+);
